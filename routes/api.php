fd6bf5ac20c9e1cb682b9bc997de777c78ccdad0
lucas.leveneur <lucas.leveneur@nautilus.fr>
2021-02-02 14:53:27 +0400

    some roots added

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use app\Http\Controllers\ordinateursController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\VendorsController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,11 +22,26 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
     Route::post('register', [AuthController::class, 'register']);
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix'     => 'vendors'
+], function ($router) {
+    Route::post('add', [VendorsController::class, 'add_vendor']);
+    Route::post('update', [VendorsController::class, 'updateShopName']);
+    Route::post('delete', [VendorsController::class, 'delete']);
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix'     => 'products'
+], function ($router) {
+    Route::post('add', [ProductsController::class, 'add']);
 });
 
