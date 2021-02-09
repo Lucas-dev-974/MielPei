6ab437e62cdb892b6394ee4a8a726a0f531e24d5
lucas.leveneur <lucas.leveneur@nautilus.fr>
2021-02-09 10:12:12 +0400

    login.vue update better

--- resources/js/views/login.vue
+++ resources/js/views/login.vue
@@ -1,22 +1,25 @@
 <template>
      <v-container class="">
          <v-row>
-            <v-alert class="col-5 my-5 mx-auto" v-model="alert" close-text="Close Alert" small type="warning" outlined dismissible>
+            <v-alert class="col-5 my-5" style="position: absolute; left: 30%;" v-model="alert" close-text="Close Alert" small type="warning" outlined dismissible>
                 {{ alert_msg }}
             </v-alert>
          </v-row>
 
          <v-row>
-             <v-card class="col-5 mx-auto" style='margin-top: 15%;' outlined >
-                 
-                <v-form class="col-5 mx-auto ">
+             <v-card class="col-5 mx-auto" style='margin-top: 10%;' outlined >
+                <p class="text-center fs-4 mt-2">Connectez-vous sur MielPeï</p>
+                <v-form class="col-10 mx-auto mb-5">
                     <v-text-field v-model="email" label="Email" class="bg-transparent">
                     </v-text-field>
 
                     <v-text-field type="password" v-model="password" label="Password">
                     </v-text-field>
-
-                    <v-btn block @click='login'>Se connecter</v-btn>
+                    <div class="d-flex justify-content-around ">
+                        <v-btn color="warning"  href="/register">S'enregistrer</v-btn>
+                        <v-btn color="success" @click='login'>Se connecter</v-btn>
+                    </div>
+                    
                 </v-form> 
              </v-card>
 
